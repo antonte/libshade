0eb21b25aebafb91defad29266fc7f5f0ebd4e32
Anton Te <te.anton@gmail.com>
2018-09-01 02:55:50 -0700

    Load font config

--- text.cpp
+++ text.cpp
@@ -1,13 +1,45 @@
 #include "text.hpp"
 #include "array_buffer.hpp"
-#include "library.hpp"
 #include "gl_check_error.hpp"
+#include "library.hpp"
+#include <cpptoml/cpptoml.h>
+#include <log/log.hpp>
 #include <vector>
 
-Text::Text(Library &library, const std::string &fontTexture, int aAspectWidth, int aAspectHeight)
-  : texture(library.getTexture(fontTexture)), aspectWidth(aAspectWidth), aspectHeight(aAspectHeight)
-
+Text::Text(Library &library, const std::string &fontTexture)
+  : texture(library.getTexture(fontTexture))
 {
+  texture->glBind(nullptr, nullptr);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glGenerateMipmap(GL_TEXTURE_2D);
+  try
+  {
+    auto &&conf = cpptoml::parse_file("data/" + fontTexture + ".toml");
+    auto &&charCoords = conf->get_array_of<cpptoml::array>("CharCoords");
+    if (!charCoords)
+      throw std::runtime_error("Invalid file format: " + fontTexture);
+    width = *conf->get_as<int64_t>("width");
+    height = *conf->get_as<int64_t>("height");
+    for (auto &&ch : *charCoords)
+    {
+      auto &&ints = ch->get_array_of<int64_t>();
+      if (!ints || ints->size() != 4)
+        throw std::runtime_error("Invalid file format, expected 4 ints: " + fontTexture);
+      coords.push_back(std::make_tuple((*ints)[0], (*ints)[1], (*ints)[2], (*ints)[3]));
+    }
+  }
+  catch (std::runtime_error &e)
+  {
+    LOG(e.what());
+  }
+  if (!coords.empty())
+    return;
+  for (int ch = ' '; ch <= '~'; ++ch)
+    coords.push_back(std::make_tuple(
+      (ch - ' ') % 16 * width / 16, (ch - ' ') / 16 * height / 6, width / 16, height / 6));
 }
 
 void Text::setText(const std::string &text)
@@ -17,23 +49,30 @@ void Text::setText(const std::string &text)
   auto x = 0.0f;
   for (auto ch : text)
   {
+    auto idx = ch - ' ';
+    if (idx >= static_cast<int>(coords.size()))
+      continue;
+    auto &&crd = coords[idx];
+    auto normWidth = 1.0f * std::get<2>(crd) / std::get<3>(crd);
     tmpVertices.emplace_back(x, 0.0f, 0.0f);
-    tmpVertices.emplace_back(x + 1.0f, 0.0f, 0.0f);
-    tmpVertices.emplace_back(x + 1.0f, 1.0f * aspectHeight / aspectWidth, 0.0f);
+    tmpVertices.emplace_back(x + normWidth, 0.0f, 0.0f);
+    tmpVertices.emplace_back(x + normWidth, 1.0f, 0.0f);
 
     tmpVertices.emplace_back(x, 0.0f, 0.0f);
-    tmpVertices.emplace_back(x + 1.0f, 1.0f * aspectHeight / aspectWidth, 0.0f);
-    tmpVertices.emplace_back(x, 1.0f * aspectHeight / aspectWidth, 0.0f);
-    auto chX = 1.0f * (ch % 16) / 16.0f;
-    auto chY = 1.0f * std::max(0, std::min(6, ch / 16 - 2)) / 6.0f;
+    tmpVertices.emplace_back(x + normWidth, 1.0f, 0.0f);
+    tmpVertices.emplace_back(x, 1.0f, 0.0f);
+    auto chX = 1.0f * std::get<0>(crd) / width;
+    auto chY = 1.0f * std::get<1>(crd) / height;
+    auto chW = 1.0f * std::get<2>(crd) / width;
+    auto chH = 1.0f * std::get<3>(crd) / height;
     tmpUvs.emplace_back(chX, chY);
-    tmpUvs.emplace_back(chX + 1.0f / 16.0f, chY);
-    tmpUvs.emplace_back(chX + 1.0f / 16.0f, chY + 1.0f / 6.0f);
+    tmpUvs.emplace_back(chX + chW, chY);
+    tmpUvs.emplace_back(chX + chW, chY + chH);
 
     tmpUvs.emplace_back(chX, chY);
-    tmpUvs.emplace_back(chX + 1.0f / 16.0f, chY + 1.0f / 6.0f);
-    tmpUvs.emplace_back(chX, chY + 1.0f / 6.0f);
-    x += 1.0f;
+    tmpUvs.emplace_back(chX + chW, chY + chH);
+    tmpUvs.emplace_back(chX, chY + chH);
+    x += normWidth - 27.0f / std::get<3>(crd);
   }
   n = tmpVertices.size();
   vertices = std::make_unique<ArrayBuffer>(tmpVertices.data(), tmpVertices.size(), 0);
--- text.hpp
+++ text.hpp
@@ -7,15 +7,16 @@ class Library;
 class Text
 {
 public:
-  Text(Library &, const std::string &fontTexture, int aspectWidth, int aspectHeight);
+  Text(Library &, const std::string &fontTexture);
   void setText(const std::string &);
   void draw();
 
 private:
   sdl::Texture *texture;
-  int aspectWidth;
-  int aspectHeight;
   std::unique_ptr<ArrayBuffer> vertices;
   std::unique_ptr<ArrayBuffer> uvs;
   int n;
+  std::vector<std::tuple<int, int, int, int>> coords;
+  int width;
+  int height;
 };
